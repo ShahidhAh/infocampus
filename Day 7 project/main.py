7b3be598eed1bb2c6960c147ac18c7294343179a
Shahidh Ahammed <162737052+ShahidhAh@users.noreply.github.com>
2026-01-13 16:39:54 +0530

    Add files via upload

---
+++ Day 7 project/main.py
@@ -0,0 +1,10 @@
+import math_utils as mp
+
+a=int(input("number a:"))
+b=int(input("number b:"))
+
+res1=mp.add(a,b)
+res2=mp.subtract(a,b)
+res3=mp.multiply(a,b)
+
+print("Addition:",res1,"Subtraction:",res2,"Multiplication:",res3)
